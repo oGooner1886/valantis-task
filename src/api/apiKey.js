f6406d03ab012107b504d5602359f87ff01ebcf6
Sergey Pokusaev <work.gooner1886@gmail.com>
2024-03-10 21:18:29 +0400

    gh-pages test

--- src/api/apiKey.js
+++
@@ -1,6 +0,0 @@
-const md5 = require("md5");
-const pass = "Valantis";
-const timeStamp = new Date().toJSON().slice(0, 10).split("-").join("");
-export const secretKey = md5(`${pass}_${timeStamp}`);
-
-
